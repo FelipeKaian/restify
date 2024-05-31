7cfe8f1cc58fb1f93d763b00142c4c964e884602
FelipeKaian <felipekaianmutti@gmail.com>
2024-05-31 12:46:50 -0300

    wip

--- lib/restify.dart
+++ lib/restify.dart
@@ -1,14 +1,29 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 library restify;
 
 import 'dart:convert';
-import 'dart:io';
+
 import 'package:http/http.dart' as http;
 
 class Restify {
-  static Future<T?> get<T>(String url,
+  String baseUrl;
+  bool decodeUTF8;
+  String? _authorization;
+
+  Restify(
+    this.baseUrl, {
+    this.decodeUTF8 = false,
+  });
+
+  void setAuthorization(String authorization) {
+    _authorization = authorization;
+  }
+
+  Future<T> get<T>(String url,
       {Map<String, String>? headers,
       Map<String, dynamic>? queryParams,
       dynamic body,
+      MultipartFile? multipartFile,
       T Function(dynamic json)? fromMap}) async {
     return request(
       url,
@@ -16,13 +31,16 @@ class Restify {
       headers: headers,
       body: body,
       fromMap: fromMap,
+      queryParams: queryParams,
+      multipartFile: multipartFile,
     );
   }
 
-  static Future<T?> post<T>(String url,
+  Future<T> post<T>(String url,
       {Map<String, String>? headers,
       Map<String, dynamic>? queryParams,
       dynamic body,
+      MultipartFile? multipartFile,
       T Function(dynamic json)? fromMap}) async {
     return request(
       url,
@@ -30,13 +48,16 @@ class Restify {
       headers: headers,
       body: body,
       fromMap: fromMap,
+      queryParams: queryParams,
+      multipartFile: multipartFile,
     );
   }
 
-  static Future<T?> put<T>(String url,
+  Future<T> put<T>(String url,
       {Map<String, String>? headers,
       Map<String, dynamic>? queryParams,
       dynamic body,
+      MultipartFile? multipartFile,
       T Function(dynamic json)? fromMap}) async {
     return request(
       url,
@@ -44,13 +65,16 @@ class Restify {
       headers: headers,
       body: body,
       fromMap: fromMap,
+      queryParams: queryParams,
+      multipartFile: multipartFile,
     );
   }
 
-  static Future<T?> delete<T>(String url,
+  Future<T> delete<T>(String url,
       {Map<String, String>? headers,
       Map<String, dynamic>? queryParams,
       dynamic body,
+      MultipartFile? multipartFile,
       T Function(dynamic json)? fromMap}) async {
     return request(
       url,
@@ -58,13 +82,16 @@ class Restify {
       headers: headers,
       body: body,
       fromMap: fromMap,
+      queryParams: queryParams,
+      multipartFile: multipartFile,
     );
   }
 
-  static Future<T?> patch<T>(String url,
+  Future<T> patch<T>(String url,
       {Map<String, String>? headers,
       Map<String, dynamic>? queryParams,
       dynamic body,
+      MultipartFile? multipartFile,
       T Function(dynamic json)? fromMap}) async {
     return request(
       url,
@@ -72,89 +99,135 @@ class Restify {
       headers: headers,
       body: body,
       fromMap: fromMap,
+      queryParams: queryParams,
+      multipartFile: multipartFile,
     );
   }
 
-  static Future<T?> request<T>(String url,
+  Future<T> request<T>(String url,
       {required RequestType requestType,
       Map<String, String>? headers,
       Map<String, dynamic>? queryParams,
       dynamic body,
+      MultipartFile? multipartFile,
       T Function(dynamic json)? fromMap}) async {
     try {
-
-      print("calling " + url);
-
-      if (headers == null) {
-        headers = <String, String>{};
-      }
+      headers ??= <String, String>{};
 
       headers['Content-Type'] = 'application/json';
+
+      if (_authorization != null) {
+        headers['Authorization'] = _authorization!;
+      }
+
+      url = baseUrl + url;
 
       if (queryParams != null) {
         url += queryParamsConverter(queryParams);
       }
 
-      http.Response response;
-
-      switch (requestType) {
-        case RequestType.GET:
-          response = await http.get(
-            Uri.parse(url),
-            headers: headers,
-          );
-          break;
-        case RequestType.POST:
-          response = await http.post(
-            Uri.parse(url),
-            headers: headers,
-            body: body,
-          );
-          break;
-        case RequestType.PUT:
-          response = await http.put(
-            Uri.parse(url),
-            headers: headers,
-            body: body,
-          );
-          break;
-        case RequestType.DELETE:
-          response = await http.delete(
-            Uri.parse(url),
-            headers: headers,
-            body: body,
-          );
-          break;
-        case RequestType.PATCH:
-          response = await http.patch(
-            Uri.parse(url),
-            headers: headers,
-            body: body,
-          );
-          break;
+      if (body is! String) {
+        body = json.encode(body);
       }
 
-      print("Respondeu -> " + response.body.toString());
-
-      if (response.statusCode >= 200 && response.statusCode < 300) {
-        String responseObject = response.body;
-        if (fromMap != null) {
+      if (multipartFile != null) {
+        var request =
+            http.MultipartRequest(requestType.toString(), Uri.parse(url));
+        request.files.add(multipartFile);
+        http.StreamedResponse streamedResponse = await request.send();
+        String responseObject = await streamedResponse.stream.bytesToString();
+        if (streamedResponse.statusCode >= 200 &&
+            streamedResponse.statusCode < 300) {
           dynamic jsonData;
-          try {
-            jsonData =
-                fromMap(json.decode(responseObject));
+          if (fromMap != null) {
+            if (decodeUTF8) {
+              try {
+                responseObject = utf8.decode(responseObject.codeUnits);
+              } catch (e) {
+                throw Exception("Erro na conversão do UTF8: $e");
+              }
+            }
+            try {
+              jsonData = fromMap(json.decode(responseObject));
+              print(
+                  "calling $url -> ${responseObject.length > 1000 ? "${responseObject.substring(0, 1000)}..." : responseObject}");
+              return jsonData;
+            } catch (e) {
+              throw Exception("Erro na conversão do body: $e");
+            }
+          } else {
             return jsonData;
-          } catch (e) {
-            throw Exception("Erro na conversão do body: " + e.toString());
           }
         } else {
-          return null;
+          throw Exception(responseObject);
         }
       } else {
-        throw Exception(response.statusCode.toString());
+        http.Response response;
+        switch (requestType) {
+          case RequestType.GET:
+            response = await http.get(
+              Uri.parse(url),
+              headers: headers,
+            );
+            break;
+          case RequestType.POST:
+            response = await http.post(
+              Uri.parse(url),
+              headers: headers,
+              body: body,
+            );
+            break;
+          case RequestType.PUT:
+            response = await http.put(
+              Uri.parse(url),
+              headers: headers,
+              body: body,
+            );
+            break;
+          case RequestType.DELETE:
+            response = await http.delete(
+              Uri.parse(url),
+              headers: headers,
+              body: body,
+            );
+            break;
+          case RequestType.PATCH:
+            response = await http.patch(
+              Uri.parse(url),
+              headers: headers,
+              body: body,
+            );
+            break;
+        }
+
+        if (response.statusCode >= 200 && response.statusCode < 300) {
+          String responseObject = response.body;
+          dynamic jsonData;
+          if (fromMap != null) {
+            if (decodeUTF8) {
+              try {
+                responseObject = utf8.decode(responseObject.codeUnits);
+              } catch (e) {
+                throw Exception("Erro na conversão do UTF8: $e");
+              }
+            }
+            try {
+              jsonData = fromMap(json.decode(responseObject));
+              print(
+                  "calling $url -> ${response.body.length > 1000 ? "${response.body.substring(0, 1000)}..." : response.body}");
+              return jsonData;
+            } catch (e) {
+              throw Exception("Erro na conversão do body: $e");
+            }
+          } else {
+            return jsonData;
+          }
+        } else {
+          throw Exception(response);
+        }
       }
     } catch (e) {
-      throw Exception("Retify error: " + e.toString());
+      throw Exception("Retify error: $e");
     }
   }
 
@@ -167,11 +240,18 @@ class Restify {
       if (queryParams.isNotEmpty) {
         queryParams += '&';
       }
-      queryParams += '$chave=$valor';
+      if (valor is String) {
+        queryParams += '$chave=${valor}';
+      } else {
+        queryParams += '$chave=${jsonEncode(valor)}';
+      }
     });
-    return queryParams;
+    return Uri.encodeFull(queryParams);
   }
 }
 
-enum RequestType { POST, PUT, GET, DELETE, PATCH }
+class MultipartFile extends http.MultipartFile{
+  MultipartFile(super.field, super.stream, super.length);
+}
 
+enum RequestType { POST, PUT, GET, DELETE, PATCH }
